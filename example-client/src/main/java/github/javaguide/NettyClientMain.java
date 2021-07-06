47287cbc39e71187ef1ce55cfc1cb37835449ba8
dazhou <1255246112@qq.com>
2021-07-06 22:48:54 +0800

    测试git

--- example-client/src/main/java/github/javaguide/NettyClientMain.java
+++ example-client/src/main/java/github/javaguide/NettyClientMain.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
  * @author shuang.kou
  * @createTime 2020年05月10日 07:25:00
  */
+
 @RpcScan(basePackage = {"github.javaguide"})
 public class NettyClientMain {
     public static void main(String[] args) throws InterruptedException {
